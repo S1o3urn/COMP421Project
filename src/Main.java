89e23560a65dd606bd5cf6e5a830f433404f0491
s1o3urn <d4vid4pplepie@gmail.com>
2020-03-30 12:42:50 -0400

    program load menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    System.out.println(
+	              " ______    __      ______   ___    ____   __    __   __    __  ____    \n"
+                + "|   ___|  /  \\    |  __  \\ |   \\  /    | |  |  |  | |  |  |  ||  _ \\  \n"
+                + "|  |__   /  _ \\   | |__\\ | |    \\/     | |  |__|  | |  |  |  || |/  /  \n"
+                + "|   __| /  /_\\ \\  |      / |  |\\   /|  | |   __   | |  |  |  ||  _  \\ \n"
+                + "|  |   /  ____\\ \\ |  |\\ \\  |  | \\_/ |  | |  |  |  | \\  \\_/  / | |_|  | \n"
+                + "|__|  /__/     \\_\\|__| \\_\\ |__|     |__| |__|  |__|  \\____ /  |_____/  \n"
+                );
+	    System.out.println("WELCOME TO FARMHUB\n");
+        System.out.println("PRESS ENTER TO PROCEED...\n");
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+
+
+
     }
 }
